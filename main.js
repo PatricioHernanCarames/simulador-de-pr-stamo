3d0a81318bea17f73fb3594f66b8d2f541efb274
Patricio Carames <patriciocarames@gmail.com>
2021-11-25 20:59:08 -0300

    se agrego simulador de prestamo y append

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ let danger = document.querySelector(".dangerAlert");
 let dolar = document.querySelector(".dolar");
 let urlDolar = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
 
-
 // General
 buttons[1].addEventListener("click", (e) => {
   alerta.classList.add("alertaFadeIn");
@@ -45,6 +44,20 @@ buttons[1].addEventListener("click", (e) => {
   } else {
     danger.style.opacity = 1;
   }
+
+  let cuota = parseInt(inputs[1].value);
+  let monto = parseInt(inputs[0].value);
+  console.log(cuota);
+  console.log(monto);
+  
+  let costoFinal= monto*1.7*1.21;
+  
+  let cuotaPrestamo = costoFinal/cuota;
+  console.log(costoFinal);
+  console.log(cuotaPrestamo);
+  $(".alert").append(`<p>Tu cuota mensual es de ${cuotaPrestamo} $</p>`);
+  $(".alert").append(`<p>El costo total de tu prestamo es de ${costoFinal} $</p>`);
+  
 });
 
 buttons[0].addEventListener("click", () => {
@@ -59,27 +72,18 @@ function showAlerta(nuevoCliente) {
     console.log(nuevoCliente[prop]);
     alerta.children[it].textContent = nuevoCliente[prop];
 
-    if (it == 5)
-      alerta.children[it].textContent = `${nuevoCliente[prop]} cuotas`;
+    if (it == 5) alerta.children[it].textContent = `${nuevoCliente[prop]}`;
     it++;
   }
   alerta.style.opacity = 1;
 }
 
-
-
-
 $(buttons[2]).click(function () {
- //let dolar=querySelector(".dolar");
-
   $.get(urlDolar, function (datos) {
-    
     let dolarOfCompra;
     let dolarOfVenta;
     for (let i = 0; i < datos.length; i++) {
-      
       for (let valor in datos[0]) {
-        
         dolarOfCompra = datos[0][valor]["compra"];
         dolarOfVenta = datos[0][valor]["venta"];
       }
@@ -89,9 +93,8 @@ $(buttons[2]).click(function () {
 
     $(".DC").html("Dolar oficial compra: " + dolarOfCompra);
     $(".DV").html("Dolar oficial venta: " + dolarOfVenta);
-    
-
   });
 });
-
- 
+// calculador de prestamo
+function calcularPrestamo() {}
+$(".btn").click(calcularPrestamo());
